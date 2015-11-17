8d1247c4aac8fa173ec36ca8f0f1166f9d4d8343
Ladislav Vondracek <lawondyss@gmail.com>
2015-11-17 13:31:31 +0100

    Added method for create link to image

--- src/Imager/ImageFactory.php
+++ src/Imager/ImageFactory.php
@@ -6,6 +6,9 @@
 
 namespace Imager;
 
+use Imager\Latte\Macro;
+use Nette\Application;
+
 class ImageFactory
 {
 
@@ -65,6 +68,41 @@ class ImageFactory
   public function createInfo($imageName)
   {
     return new ImageInfo($imageName);
+  }
+
+
+  /**
+   * Returns URL for thumbnail of image
+   *
+   * @param \Nette\Application\IPresenter $presenter
+   * @param string $imageName
+   * @param null|int $width
+   * @param null|int $height
+   * @return string
+   * @throws \Imager\RuntimeException
+   */
+  public function createLink(Application\IPresenter $presenter, $imageName, $width = null, $height = null)
+  {
+    // Parameters $presenter and $baseUrl it's in code for eval
+    // Their presence here enables functionality
+
+    $baseUrl = $presenter->context->getService('http.request')->getUrl()->baseUrl;
+
+    // code with parameters for eval
+    $parameters = sprintf('["%s", %s, %s]', $imageName, $width ?: 'null', $height ?: 'null');
+
+    $code = Macro::getCode($parameters);
+
+    // for get link must be result returns
+    $code[] = 'return $link;';
+
+    $link = @eval(implode('', $code)); // @ escalates to exception
+    if (!isset($link) || $link === false) {
+      $msg = sprintf('Error on code for eval(): %s', implode(' ', $code));
+      throw new RuntimeException($msg);
+    }
+
+    return $link;
   }
 
 
